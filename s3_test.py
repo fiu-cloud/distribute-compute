584ab2f0845379220b0a4fa1693eed3bb7c33f41
michaelwalker <michael.walker@officespark.com>
2018-08-15 15:08:36 +0900

    I/O non functionally works

--- s3_test.py
+++ s3_test.py
@@ -12,3 +12,5 @@ cur.execute("CREATE WRITABLE EXTERNAL TABLE MOCK2 (LIKE MOCK) LOCATION('s3://s3-
 cur.execute("INSERT INTO MOCK2 SELECT * FROM MOCK")
 cur.close()
 
+#CREATE WRITABLE EXTERNAL TABLE ABC (value float8) LOCATION('s3://s3-ap-southeast-2.amazonaws.com/ifs-cloud/tmp config=/home/gpadmin/s3.conf') FORMAT 'csv'
+#INSERT INTO ABC SELECT random() as value FROM generate_series(1,1000)
--- src/main/python/mapping.py
+++ src/main/python/mapping.py
@@ -1,4 +1,4 @@
-import phe.paillier as paillier, psycopg2,uuid,time,numpy as np
+import phe.paillier as paillier, psycopg2,uuid,time,psycopg2.extras
 
 _host="localhost"
 _database="gpadmin"
@@ -9,10 +9,7 @@ _s3_bucket="ifs-cloud"
 _s3_finished="_finished_"
 pubkey, prikey = paillier.generate_paillier_keypair(n_length=1024)
 
-#read
-#schema is numphy schema
-#returns numphy table
-def read(file, schema1, schema2):
+def read(file, schema1):
     conn = psycopg2.connect(host=_host,database=_database, user=_user, password=_password)
     # poll status table
     table = "tbl"+str(uuid.uuid1()).replace("-","_")
@@ -33,34 +30,46 @@ def read(file, schema1, schema2):
     cur = conn.cursor()
     cur.execute("CREATE READABLE EXTERNAL TABLE "+table+" "+schema1+" LOCATION ('s3://"+_s3_endpoint+"/"+_s3_bucket+"/"+file+" config=/home/gpadmin/s3.conf') FORMAT 'csv'")
     cur.execute("SELECT * FROM "+table)
-    table = cur.fetchall()
-    table = table[1:]
-    out = np.array(table,dtype=schema2)
+    out = cur.fetchall()
+
     cur.close()
     conn.close()
     return out
+# arr = np.matrix([[1, 2, 3 , "a"], [5,6,7,"bc"]])
 
-
-def write(upload, file, schema1, schema2):
+def write(upload, file, schema):
     conn = psycopg2.connect(host=_host,database=_database, user=_user, password=_password)
     # poll status table
     poll_file = "_finished_"+file
     cur = conn.cursor()
 
     #Create tables
-    pollTable = "tbl"+str(uuid.uuid1()).replace("-","_")
-    cur.execute("CREATE WRITABLE EXTERNAL TABLE "+pollTable+" (value text) LOCATION ('s3://"+_s3_endpoint+"/"+_s3_bucket+"/"+poll_file+" config=/home/gpadmin/s3.conf') FORMAT 'csv'")
-    table = "tbl"+str(uuid.uuid1()).replace("-","_")
-    cur.execute("CREATE WRITABLE EXTERNAL TABLE "+table+" "+schema1+" LOCATION ('s3://"+_s3_endpoint+"/"+_s3_bucket+"/"+file+" config=/home/gpadmin/s3.conf') FORMAT 'csv'")
+    id = str(uuid.uuid1()).replace("-","_")
+    print("USING "+id)
+    pollTable = "poll_"+id
+    table = "s3_"+id
+    stagingTable = "staging_"+id
 
-    #upload data
-    for row in upload:
-        cur.execute("INSERT INTO "+table+" "+schema2,row)
+    cur.execute("CREATE WRITABLE EXTERNAL TABLE "+pollTable+" (value text) LOCATION ('s3://"+_s3_endpoint+"/"+_s3_bucket+"/"+poll_file+" config=/home/gpadmin/s3.conf') FORMAT 'csv'")
+    cur.execute("CREATE WRITABLE EXTERNAL TABLE "+table+" "+schema+" LOCATION ('s3://"+_s3_endpoint+"/"+_s3_bucket+"/"+file+" config=/home/gpadmin/s3.conf') FORMAT 'csv'")
+    cur.execute("CREATE TABLE "+stagingTable+" "+schema)
+
+    psycopg2.extras.execute_values(cur, "insert into "+stagingTable+" VALUES %s", upload, template=None, page_size=1000)
     cur.execute("INSERT INTO "+pollTable+" (value) VALUES (%s)","1")
+    cur.execute("INSERT INTO "+table + " SELECT * FROM " + stagingTable)
+
+    conn.commit() #do we need this
     cur.close()
 
-
-
-table = read("mock2.csv","(key text, value text)",[('key', 'U10'), ('value', 'U10')])
-write(table,"mock3.csv","(key text, value text)","(key, value) VALUES (%s, %s)")
+def scenario(size):
+    schema = "(a text, b integer, c float8)"
+    in_tbl = []
+    for i in range(0, size, 1):
+        in_tbl.append([str(i),i,i+0.5])
+    write(in_tbl,"A",schema)
+    out = read("A",schema)
+    write(out,"B",schema)
+    out1 = read("B",schema)
+    print(len(out1))
+scenario(1000000)
 
