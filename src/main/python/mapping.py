0f549cd87fe7f83f15a32a87ae5cbfe8ea74b4b4
michaelwalker <michael.walker@officespark.com>
2018-08-22 17:55:55 +0900

    encrypted addition works

--- src/main/python/mapping.py
+++ src/main/python/mapping.py
@@ -1,4 +1,4 @@
-import phe.paillier as paillier, psycopg2,uuid,time,psycopg2.extras,sys
+import phe.paillier as paillier, psycopg2,uuid,time,psycopg2.extras,sys,pickle,binascii,datetime
 
 _host="localhost"
 _database="gpadmin"
@@ -7,7 +7,7 @@ _password="greenplum"
 _s3_endpoint="s3-ap-southeast-2.amazonaws.com"
 _s3_bucket="alerting-project"
 _s3_finished="_finished_"
-pubkey, prikey = paillier.generate_paillier_keypair(n_length=1024)
+
 
 def read(file, schema1,i):
     conn = psycopg2.connect(host=_host,database=_database, user=_user, password=_password)
@@ -37,7 +37,7 @@ def read(file, schema1,i):
     cur.execute("CREATE READABLE EXTERNAL TABLE "+table+" "+schema1+" LOCATION ('s3://"+_s3_endpoint+"/"+_s3_bucket+"/"+data_file+" config=/home/gpadmin/s3.conf') FORMAT 'csv'")
     cur.execute("SELECT * FROM "+table)
     out = cur.fetchall()
-    print(str(i) + " : READ ROWS "+id + " / "+ file + " COUNT="+str(len(out)))
+    print(str(datetime.datetime.now()) + " "+str(i) + " : READ ROWS "+id + " / "+ file + " COUNT="+str(len(out)))
 
     cur.close()
     conn.close()
@@ -63,55 +63,73 @@ def write(upload, file, schema,i):
     cur.execute("CREATE TABLE "+stagingTable+" "+schema)
 
     psycopg2.extras.execute_values(cur, "insert into "+stagingTable+" VALUES %s", upload, template=None, page_size=1000)
+
     cur.execute("INSERT INTO "+table + " SELECT * FROM " + stagingTable)
     conn.commit()
     cur.execute("INSERT INTO "+pollTable+" (value) VALUES (%s)","1")
     conn.commit()
-    print(str(i) + " : WROTE ROWS "+id + " / "+ file + " COUNT="+str(len(upload)))
+    print(str(datetime.datetime.now()) + " "+str(i) + " : WROTE ROWS "+id + " / "+ file + " COUNT="+str(len(upload)))
     conn.commit()
     cur.close()
 
+def serialise(input):
+    return binascii.b2a_base64(pickle.dumps(input)).decode('ascii')
+
+def deserialise(input):
+    return pickle.loads(binascii.a2b_base64(input.encode('ascii')))
+
 def additionScenario():
     iterations = 10
-    schema = "(a text, b integer, c float8)"
+    schema = "(a text, b text)"
     rowCount = 1000000
     party = sys.argv[1]
+    print(">> "+ party)
 
     if party == "a":
+        #process keys
+        pubkey, prikey = paillier.generate_paillier_keypair(n_length=1024)
+        key = serialise(pubkey)
+        write([[key]], "pubkey/", "(key text)",-1)
+
+        #write the initial data
         in_tbl = []
         for i in range(0, rowCount, 1):
-            in_tbl.append([str(i),i,i+0.5])
+            in_tbl.append([str(i),serialise(pubkey.encrypt(i))])
         write(in_tbl,"a/0/",schema,-1)
-
+    elif party == "b":
+        #process keys
+        pubkey = deserialise((read("pubkey/", "(key text)",-1))[0][0])
     for i in range(0, iterations):
         if party == "a":
             temp = read("b/"+str(i)+"/",schema,i)
-            out = list(map(lambda x: [x[0], x[1]+1, x[2]+1], temp))
+            out = list(map(lambda x: [x[0], serialise(deserialise(x[1])+pubkey.encrypt(1))], temp))
             # print(str(i) + " : " + str(temp) + " -> " + str(out))
-
             write(out,"a/"+str(i+1)+"/",schema,i)
         elif party == "b":
             temp = read("a/"+str(i)+"/",schema,i)
-            out = list(map(lambda x: [x[0], x[1]+1, x[2]+1], temp))
+            out = list(map(lambda x: [x[0], serialise(deserialise(x[1])+pubkey.encrypt(1))], temp))
             # print(str(i) + " : " + str(temp) + " -> " + str(out))
             write(out,"b/"+str(i)+"/",schema,i)
-
-
     print("FINISHED")
-    outA = read("a/"+str(iterations)+"/",schema,-1)
-    print("A "+str(len(outA)))
-    outB = read("b/"+str(iterations - 1)+"/",schema,-1)
-    print("B "+str(len(outB)))
 
-def scenario(size):
-    schema = "(a text, b integer, c float8)"
-    in_tbl = []
-    for i in range(0, size, 1):
-        in_tbl.append([str(i),i,i+0.5])
-    write(in_tbl,"A",schema)
-    out = read("A",schema)
-    write(out,"B",schema)
-    out1 = read("B",schema)
-    print(len(out1))
+    if party == "a":
+        outA = read("a/"+str(iterations)+"/",schema,-1)
+        print("A "+str(len(outA)))
+        totalA = 0
+        for i in outA:
+            value = prikey.decrypt(deserialise(i[1]))
+            totalA += value
+        #    print("A " + str(i[0]) + " -> "+ str(value))
+        print("A total " + str(totalA))
+
+        outB = read("b/"+str(iterations - 1)+"/",schema,-1)
+        print("A "+str(len(outB)))
+        totalB = 0
+        for i in outB:
+            value = prikey.decrypt(deserialise(i[1]))
+            totalB += value
+        #    print("B " + str(i[0]) + " -> "+ str(value))
+        print("B total " + str(totalB))
+
 
 additionScenario()
--- src/main/python/serialisation_tests.py
+++ src/main/python/serialisation_tests.py
@@ -1,11 +1,13 @@
-import phe.paillier as paillier, math, pickle
+import phe.paillier as paillier, math, pickle, binascii
 pubkey, prikey = paillier.generate_paillier_keypair(n_length=1024)
-print(pickle.dumps(pubkey))
+text = binascii.b2a_hex(pickle.dumps(pubkey)).decode('ascii')
+print(text)
+binary = binascii.a2b_hex(text.encode('ascii'))
+pubkey = pickle.loads(binary)
+encrypted = pubkey.encrypt(math.pi)
+pickledEncrypted = binascii.b2a_base64(pickle.dumps(encrypted))
+encrypted2 = pickle.loads(binascii.a2b_base64(pickledEncrypted))
+decrypted = prikey.decrypt(encrypted)
 
-#encrypted = pubkey.encrypt(math.pi)
-#pickledEncrypted = pickle.dumps(encrypted)
-#encrypted2 = pickle.loads(pickledEncrypted)
-#decrypted = prikey.decrypt(encrypted2)
-
-#print(type(encrypted2))
-#print(decrypted)
+print(type(decrypted))
+print(decrypted)
