61db1e3adf9fed0663aa81426d279892ad8b563b
michaelwalker <michael.walker@officespark.com>
2018-08-28 16:30:16 +0900

    linear regression

---
+++ src/main/python/linear_regression.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+
+# For 3d plots. This import is necessary to have 3D plotting below
+from mpl_toolkits.mplot3d import Axes3D
+
+# For statistics. Requires statsmodels 5.0 or more
+from statsmodels.formula.api import ols
+# Analysis of Variance (ANOVA) on linear models
+from statsmodels.stats.anova import anova_lm
+
+init = np.linspace(-5, 5, 20)
+
+# We generate a 2D grid
+x1, x2 = np.meshgrid(init, init)
+
+# To get reproducable values, provide a seed value
+np.random.seed(1)
+
+# Z is the elevation of this 2D grid
+y = 3*x1 - 0.5*x2 + np.random.normal(size=x1.shape)
+
+
+x1 = x1.flatten()
+x2 = x2.flatten()
+y = y.flatten()
+
+alpha = 0.1
+n = float(len(x1))
+x1_theta = 0
+x2_theta = 0
+gradients = [0] * len(x1)
+
+
+for i in range(0, 1000):
+    print("iteration: "+str(i))
+    #predict x1
+    x1_prediction = x1 * x1_theta
+
+    #predict x2
+    x2_prediction = x1_prediction + x2 * x2_theta
+
+    #calculate gradient
+    gradient = x2_prediction - y
+
+    #update x1
+    x1_theta = x1_theta - (alpha / n) * sum(gradient * x1)
+
+    #update x2
+    x2_theta = x2_theta - (alpha / n) * sum(gradient * x2)
+
+print("x1="+ str(x1_theta) + " x2="+str(x2_theta))
